8ea18ec4343225a32b7b1c7bba0f53245b8175c6
Ahmed Adan <ahmed.adan@gmail.com>
2026-02-24 22:22:56 -0500

    bcvk 0.10.0

---
+++ Formula/bcvk.rb
@@ -0,0 +1,34 @@
+class Bcvk < Formula
+  desc "CLI tool for launching ephemeral VMs from bootc container images"
+  homepage "https://github.com/bootc-dev/bcvk"
+  url "https://github.com/bootc-dev/bcvk.git", tag: "v0.10.0", revision: "ae1f258dc93b1c46a924a27246aa3f1497258712"
+  license any_of: ["Apache-2.0", "MIT"]
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "openssl" => :build
+  depends_on "pkg-config" => :build
+  depends_on "rust" => :build
+  depends_on :linux
+
+  def install
+    system "cargo", "install", "--locked", "--path", "crates/kit", "--root", prefix
+  end
+
+  def caveats
+    <<~EOS
+      bcvk requires the following runtime dependencies:
+        podman, qemu-kvm, qemu-img, virtiofsd, openssh-clients, binutils
+
+      For libvirt integration, also install:
+        libvirt-client
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bcvk --version")
+  end
+end

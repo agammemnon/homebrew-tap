433959185f6eb1eb577af20b398bfbf104f90104
Ahmed Adan <ahmed.adan@gmail.com>
2026-04-13 09:56:33 +0000

    docker-desktop-linux 4.69.0

--- Casks/docker-desktop-linux.rb
+++ Casks/docker-desktop-linux.rb
@@ -1,5 +1,5 @@
 cask "docker-desktop-linux" do
-  version "4.68.0,223695"
+  version "4.69.0,224084"
   sha256 :no_check
 
   url "https://desktop.docker.com/linux/main/amd64/docker-desktop-x86_64.rpm"

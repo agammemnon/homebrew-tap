1bafece8225604dfa450dcaca6150d1359cffa1a
Ahmed Adan <ahmed.adan@gmail.com>
2026-05-15 02:59:09 +0000

    zed-linux 1.2.4

--- Casks/zed-linux.rb
+++ Casks/zed-linux.rb
@@ -1,6 +1,6 @@
 cask "zed-linux" do
-  version "1.1.7"
-  sha256 "e3af2c1b65307e64583e88fee527ef163c9df55682517c1db4c9a33f1aefe6d4"
+  version "1.2.4"
+  sha256 "a915fb9d943dbad25bad21ef5d54e9748315aeac431f1a51f0806c10a23faded"
 
   url "https://github.com/zed-industries/zed/releases/download/v#{version}/zed-linux-x86_64.tar.gz"
   name "Zed"

c370df00d632711b7c7d6e30de2e44fe7095c43d
Ahmed Adan <ahmed.adan@gmail.com>
2026-07-17 02:26:52 +0000

    google-chrome-linux 150.0.7871.128

--- Casks/google-chrome-linux.rb
+++ Casks/google-chrome-linux.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-linux" do
-  version "150.0.7871.124"
+  version "150.0.7871.128"
   sha256 :no_check
 
   url "https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm"

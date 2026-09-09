3f00e2966d36d83b5dfa79715d95ffb44b54d733
Ahmed Adan <ahmed.adan@gmail.com>
2026-09-09 04:40:38 -0400

    helium-browser-linux 0.16.6.1 (#567)

--- Casks/helium-browser-linux.rb
+++ Casks/helium-browser-linux.rb
@@ -1,6 +1,6 @@
 cask "helium-browser-linux" do
-  version "0.16.5.1"
-  sha256 "f615a7735663584364086a2be93f0e79ba1238a856be6ddbb5aef73e7c94a970"
+  version "0.16.6.1"
+  sha256 "1003744b4ff97797c7d3dd12da8bf74278c5f4e93e91f15b411b57afcffc2f31"
 
   url "https://github.com/imputnet/helium-linux/releases/download/#{version}/helium-#{version}-x86_64_linux.tar.xz"
   name "Helium Browser"

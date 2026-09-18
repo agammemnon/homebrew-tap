f632f3e132172e7bff4d63a17814d7ccb97eae19
Ahmed Adan <ahmed.adan@gmail.com>
2026-09-17 23:13:22 -0400

    cursor-linux 3.21.9 (#584)

--- Casks/cursor-linux.rb
+++ Casks/cursor-linux.rb
@@ -3,9 +3,9 @@ cask "cursor-linux" do
   file_arch = on_arch_conditional arm: "aarch64", intel: "x86_64"
   os linux: "linux"
 
-  version "3.19.19,6496ea8a068aebfcd21990e70ff522e9abf10c8c"
-  sha256 arm64_linux:  "952b0e7cca2365fa1c993aa0055a8317fa83445db2d318859605e153723ed92e",
-         x86_64_linux: "989ea04f86fa639dc543b6d041b14b5bf13a322ac493547af72cb66d676521de"
+  version "3.21.9,9998796a6096ce83d83a9332bfe7473b985db750"
+  sha256 arm64_linux:  "df71c8b7caa361a5131645c29295baa1d6617e40de5b07cb8cbdc7a894585960",
+         x86_64_linux: "f82c1d478107b83c87ab75301ece78898f71b90f5ca06ea05a8a5445815fa698"
 
   url "https://downloads.cursor.com/production/#{version.csv.second}/linux/#{arch}/Cursor-#{version.csv.first}-#{file_arch}.AppImage"
   name "Cursor"

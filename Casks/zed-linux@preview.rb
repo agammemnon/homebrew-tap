266848ef06391fe4d11e1090acf2448c0a4532b2
Ahmed Adan <ahmed.adan@gmail.com>
2026-09-23 22:43:53 +0000

    zed-linux@preview 1.22.0-pre

--- Casks/zed-linux@preview.rb
+++ Casks/zed-linux@preview.rb
@@ -1,8 +1,8 @@
 cask "zed-linux@preview" do
   os linux: "linux"
 
-  version "1.21.0-pre"
-  sha256 "eadc30098196a0a212988a5ea4d193e78641e17b459e4275ddd7675175449b44"
+  version "1.22.0-pre"
+  sha256 "20f1fb519ce88100511bb09ca4c3d3dd7d0898b4cea0add03fb55966db14f332"
 
   url "https://github.com/zed-industries/zed/releases/download/v#{version}/zed-linux-x86_64.tar.gz"
   name "Zed Preview"
